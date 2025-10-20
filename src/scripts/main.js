e4d9509349183a29c4fa7f445fe8afa72d37f106
Dima <dima2003strik@gmail.com>
2025-10-20 22:51:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainPicture = document.querySelector('#largeImg');
+
+const ul = document.querySelector('#thumbs');
+
+ul.addEventListener('click', (event) => {
+  const thumbnail = event.target.closest('.list-item__link');
+
+  if (!thumbnail) return;
+
+  event.preventDefault();
+
+  mainPicture.src = thumbnail.href; // ==> (.getAttribute('href'))
+});
+
